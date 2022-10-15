547b589c8f88faf79e2978277270a727595e9c14
Ilya Makarov <mortimer08@gmail.com>
2022-10-15 20:39:52 +0500

    Task08 Complete

---
+++ Task08/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 8: Напишите программу
+// которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int currentNumber = 2;
+if (number < 2)
+{
+    Console.WriteLine("Число не подходит");
+}
+else
+{
+    while (currentNumber <= number)
+    {
+        Console.WriteLine(currentNumber);
+        currentNumber = currentNumber + 2;
+    }
+}
