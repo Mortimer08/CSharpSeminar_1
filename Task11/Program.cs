f10239d48220363053071c227eecf2c18e2e5f33
Ilya Makarov <mortimer08@gmail.com>
2022-10-17 13:41:48 +0500

    Task11 Complete2 Method

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -7,8 +7,20 @@
 int number = new Random().Next(100, 1000);
 Console.WriteLine($"Случайное трёхзначное число => {number}");
 
-int digit1 = number / 100;
-int digit3 = number % 10;
+// int digit1 = number / 100;
+// int digit3 = number % 10;
 
-int newNumber = digit1*10+digit3;
+// int newNumber = digit1 * 10 + digit3;
+// Console.WriteLine($"Получилось число {newNumber}");
+
+int RemoveSecondDigit(int num)
+{
+    int res = default;
+    int digit1 = num / 100;
+    int digit3 = num % 10;
+    res = digit1 * 10 + digit3;
+    return res;
+}
+
+int newNumber = RemoveSecondDigit(number);
 Console.WriteLine($"Получилось число {newNumber}");
