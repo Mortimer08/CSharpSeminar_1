460095a4f738175a18d5e80b5132abdd20bc7e12
Ilya Makarov <mortimer08@gmail.com>
2022-10-17 13:31:57 +0500

    Task11 Complele

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное трёхзначное число
+// и удаляет из него вторую цифру
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трёхзначное число => {number}");
+
+int digit1 = number / 100;
+int digit3 = number % 10;
+
+int newNumber = digit1*10+digit3;
+Console.WriteLine($"Получилось число {newNumber}");
