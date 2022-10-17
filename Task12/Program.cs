8a6bec629bbb6c9f1b688c0aabe0f09ff959487b
Ilya Makarov <mortimer08@gmail.com>
2022-10-17 14:04:58 +0500

    Task12 Complete

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу
+
+Console.WriteLine("Введите два числа");
+Console.Write("Первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 % number2 == 0)
+{
+    Console.WriteLine($"Число {number1} кратно числу {number2}");
+}
+else
+{
+    Console.WriteLine($"Остаток от деления числа {number1} на {number2} составляет {number1 % number2}");
+}
