0c45382c289268ebf14a02a0df8bb7d28cb76702
Ilya Makarov <mortimer08@gmail.com>
2022-10-17 12:34:39 +0500

    Task09 Complete1

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -1,2 +1,7 @@
 ﻿int number = new Random().Next(10,100);
-Console
+Console.WriteLine($"Случайное число из отрезка 10 - 99 => {number}");
+int digit1 = number % 10;
+int digit2 = number / 10;
+
+if (digit1 > digit2) Console.WriteLine($"Максимальная цифра числа => {digit1}");
+else Console.WriteLine($"Максимальная цифра числа => {digit2}");
