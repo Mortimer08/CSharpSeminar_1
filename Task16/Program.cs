bff158071f69dd9b3de26228fafd1af156a8206e
Ilya Makarov <mortimer08@gmail.com>
2022-10-17 14:28:33 +0500

    Task16 Complete

---
+++ Task16/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите два числа");
+Console.Write("Первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 * number1 == number2 || number2 * number2 == number1)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
