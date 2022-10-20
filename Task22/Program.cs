fdce77eed9db3f2742eeaee9e2790e2657b2364c
Ilya Makarov <mortimer08@gmail.com>
2022-10-20 21:24:57 +0500

    Task22 Complete

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+
+System.Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Squars(int num)
+{
+    if (num > 0)
+    {
+        for (int i = 1; i <= num; i++)
+        {
+            Console.WriteLine($"{i}    {i * i}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Введено некорректное значение");
+    }
+}
+Squars(number);
