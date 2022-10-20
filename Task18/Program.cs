422e19d0d9256f365a93a6d097f20a4a51a3b0a3
Ilya Makarov <mortimer08@gmail.com>
2022-10-20 20:50:35 +0500

    Task20 Added

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -3,14 +3,29 @@
 // возможных координат точек в этой четверти (x и y).
 
 Console.WriteLine("Введите номер четверти");
-int quarter = Convert.ToInt32(Console.ReadLine());
+// int quarter = Convert.ToInt32(Console.ReadLine());
 
-string Diap(int qNumber)
+// string Diap(int qNumber)
+// {
+//     if (qNumber == 1) return "x > 0 и y > 0";
+//     if (qNumber == 2) return "x < 0 и y > 0";
+//     if (qNumber == 3) return "x < 0 и y < 0";
+//     if (qNumber == 4) return "x > 0 и y < 0";
+//     return "Введено некорректное значение";
+// }
+
+// string result = Diap(quarter);
+
+// Console.WriteLine(result);
+
+string quarter = Console.ReadLine();
+
+string Diap(string qNumber)
 {
-    if (qNumber == 1) return "x > 0 и y > 0";
-    if (qNumber == 2) return "x < 0 и y > 0";
-    if (qNumber == 3) return "x < 0 и y < 0";
-    if (qNumber == 4) return "x > 0 и y < 0";
+    if (qNumber == "1") return "x > 0 и y > 0";
+    if (qNumber == "2") return "x < 0 и y > 0";
+    if (qNumber == "3") return "x < 0 и y < 0";
+    if (qNumber == "4") return "x > 0 и y < 0";
     return "Введено некорректное значение";
 }
 
