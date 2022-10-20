2ac9236471492f2a5859b42a209b861b23e0fcfe
Ilya Makarov <mortimer08@gmail.com>
2022-10-20 20:42:10 +0500

    Task18 Complete

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+string Diap(int qNumber)
+{
+    if (qNumber == 1) return "x > 0 и y > 0";
+    if (qNumber == 2) return "x < 0 и y > 0";
+    if (qNumber == 3) return "x < 0 и y < 0";
+    if (qNumber == 4) return "x > 0 и y < 0";
+    return "Введено некорректное значение";
+}
+
+string result = Diap(quarter);
+
+Console.WriteLine(result);
