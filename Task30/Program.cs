73ff7191c5efca87bae2d55c7fcac8fa0afbf287
Ilya Makarov <mortimer08@gmail.com>
2022-11-07 21:25:32 +0500

    commit task30 processing

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -3,3 +3,11 @@
 // нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
 
+int[] array = new int[8];
+
+for (int i = 0  ; i < array.Length; i++)
+{
+    array[i] = Random().Next(0,2);
+}
+
+Console.WriteLine(array);
