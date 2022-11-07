da844fb837d762651db8427f60f9bab31730e94f
Ilya Makarov <mortimer08@gmail.com>
2022-11-07 20:42:28 +0500

    Task24 complete

--- Task24/Program.cs
+++ Task24/Program.cs
@@ -5,13 +5,13 @@
 // 4 -> 10
 // 8 -> 36
 using System;
-
+Console.Write("Введите целое число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(SumNumbers(num));
+Console.WriteLine($"Сумма чисел от 1 до {num} = {SumNumbers(num)}");
 
 int SumNumbers(int number)
 {
-    int sum = 0;
+    int sum = default;
     for (int i = 1; i < number+1; i ++)
     {
         sum += i;
