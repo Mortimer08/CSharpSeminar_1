63e3cb1852773575b124a719397eab41e348e746
Ilya Makarov <mortimer08@gmail.com>
2022-11-17 22:04:54 +0500

    Task51 Complete

---
+++ Task49/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+            Console.Write(j < matrix.GetLength(1) - 1 ? " " : "");
+        }
+        Console.WriteLine("]");
+    }
+
+}
+
+// void SquareSomeMatrixElements(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (i % 2 == 0 && j % 2 == 0) matrix[i, j] *= matrix[i, j];
+//         }
+//     }
+// }
+// Более оптимально:
+void SquareSomeMatrixElements(int[,] matrix)
+{
+    for (int i = 2; i < matrix.GetLength(0); i+=2)
+    {
+        for (int j = 2; j < matrix.GetLength(1); j+=2)
+        {
+            matrix[i, j] *= matrix[i, j];
+        }
+    }
+}
+int[,] randomMatrix = CreateMatrixRndInt(5, 5, 0, 10);
+PrintMatrix(randomMatrix);
+SquareSomeMatrixElements(randomMatrix);
+Console.WriteLine();
+PrintMatrix(randomMatrix);
+
---
+++ Task51/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1;1) и т.д.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+            Console.Write(j < matrix.GetLength(1) - 1 ? " " : "");
+        }
+        Console.WriteLine("]");
+    }
+
+}
+
+int SumElementsMatrixDiagonal(int[,] matrix)
+{
+    int sum = 0;
+    int diagonalLenth = matrix.GetLength(0) < matrix.GetLength(1) ?
+                        matrix.GetLength(0) : matrix.GetLength(1);
+    for (int i = 0; i < diagonalLenth; i++)
+    {
+        sum+=matrix[i,i];
+    }
+    return sum;
+}
+int[,] randomMatrix = CreateMatrixRndInt(5, 5, 0, 10);
+PrintMatrix(randomMatrix);
+
+int sumElementsMatrixDiagonal = SumElementsMatrixDiagonal(randomMatrix);
+System.Console.WriteLine(sumElementsMatrixDiagonal);
